85dd9ba1ea372b86a72f0c3c0325294af393cad4
TiagoSilvina <tiagoafsilvina@gmail.com>
2024-02-12 16:56:42 +0000

    footer, sidebar, notfound pages added

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import KanbanBoard from "./components/KanbanBoard";
 import Task from "./components/Task";
 import Column from "./components/Column";
 import "./App.css";
+import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Sidebar />
       {/* <Task
         task={{ id: 123, title: "Make a progress board application" }}
         index="1"
       /> */}
       <KanbanBoard />
+      <Footer />
     </div>
   );
 }
